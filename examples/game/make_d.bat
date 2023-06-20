32b34e5dc34066bc823856dcc2a36e199b065e1b
Steve Williams <steve@advance-software.com>
2023-06-20 08:11:20 +0100

    fixed make batch files.

--- examples/game/make_d.bat
+++ examples/game/make_d.bat
@@ -1,50 +1,50 @@
+@echo off
+
 rem ===============        <><> Infinity Web Assembly SDK makefile (batch file for now)
 
-rem Debug build
-set EXTRA_COMPILE_OPTIONS=-g
+rem Release build optimization level
+set EXTRA_COMPILE_OPTIONS=-g -D__wasi__
+
+
 
 rem ======= Configure =======
 
-rem WASI delivers standard c/c++ runtime libraries
+rem WASI delivers standard C/C++ runtime libraries
 rem Latest wasi-sdk here : https://github.com/WebAssembly/wasi-sdk/releases
 rem Docs : https://wasi.dev
 
+set LLVM_H=..\..\sys\include
 set WASI_LIB=..\..\sys\wasi\lib\wasm32-wasi
 set WASI_H=..\..\sys\wasi\include
 set WASI_CPP_H=..\..\sys\wasi\include\c++\v1
-set WASI_SDK=..\..\sys\wasi\sdk\bin
+set WASM_TOOLS=..\..\sys\bin
+set CPP=%WASM_TOOLS%\clang++
+
 
 rem <><> Infinity/XSG DOM interop/bootstrap headers
 set INFINITY_H=..\..\sys\infinity
 
-set COMPILE_OPTIONS=%EXTRA_COMPILE_OPTIONS% --target=wasm32 -fno-exceptions -c 
-set HEADERS=-I%WASI_H% -I%WASI_CPP_H% -I%INFINITY_H%
+set COMPILE_OPTIONS=%EXTRA_COMPILE_OPTIONS%  --target=wasm32 -fno-exceptions -c 
+set HEADERS=-I%WASI_CPP_H% -I%WASI_H% -I%LLVM_H% -I%INFINITY_H%
 
 rem ======= Compile =======
 
 if not exist obj mkdir obj
 
-%WASI_SDK%\clang %HEADERS% %COMPILE_OPTIONS% ./source/inf_entity.cpp -o ./obj/inf_entity.o 
-%WASI_SDK%\clang %HEADERS% %COMPILE_OPTIONS% ./source/program.cpp    -o ./obj/program.o 
+%CPP% %HEADERS% %COMPILE_OPTIONS% ./source/inf_entity.cpp  -o ./obj/inf_entity.o 
+%CPP% %HEADERS% %COMPILE_OPTIONS% ./source/program.cpp  -o ./obj/program.o 
 
 
 rem ======= Link =======
 
 
-set LINK_OPTIONS=-no-entry --allow-undefined --export=app_init --export=app_process --export=app_terminate --export=app_move --export=app_msg --export=app_button
+set LINK_OPTIONS=--no-entry --allow-undefined --export=app_init --export=app_process --export=app_terminate --export=app_move --export=app_msg --export=app_button
 set LIBS= %WASI_LIB%\libc.a  %WASI_LIB%\libc++abi.a   %WASI_LIB%\libc++.a 
 
-%WASI_SDK%\wasm-ld %LINK_OPTIONS%  %LIBS% ./obj/inf_entity.o ./obj/program.o  -o ./obj/app.wasm
-
-
-rem ======= Inspect =======
-
-rem optional, convert to wat (text format) for manual inspection if difficulties arise.
-rem wasm2wat available here : https://github.com/WebAssembly/wabt/releases
-rem wasm2wat ./obj/app.wasm > ./obj/app.wat
+%WASM_TOOLS%\wasm-ld %LINK_OPTIONS%  %LIBS% ./obj/inf_entity.o  ./obj/program.o -o ./obj/app.wasm
 
 
 rem ======= Install =======
 
-rem push wasm binary into appropriate website directory for execution.
+rem Push to distribution ...
 copy .\obj\app.wasm ..\..\website\game\_action
