624116fc2236103c53457d962d3a58f3206c70d7
Steve Williams <steve@advance-software.com>
2024-03-22 10:19:59 +0200

    .

--- examples/vertex/run.bat
+++ examples/vertex/run.bat
@@ -1,7 +1,6 @@
+@echo off
 
 rem ===============        <><> Infinity Web Assembly SDK run script
 
-
-rem ======= Run =======
 
 infinity.exe -alone -desktop -ui=0 -rt=dxr %cd%\..\..\website\vertex
--- examples/vertex/run_safe.bat
+++ examples/vertex/run_safe.bat
@@ -1,7 +1,6 @@
+@echo off
 
 rem ===============        <><> Infinity Web Assembly SDK run script
 
-
-rem ======= Run =======
 
 infinity.exe -alone -desktop -ui=0 %cd%\..\..\website\vertex
