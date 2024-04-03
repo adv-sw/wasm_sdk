819060f8fa440e266e6a985bf0b63571b08213e3
Steve Williams <steve@advance-software.com>
2024-04-03 17:21:48 +0300

    fixed build scripts

--- examples/visualize/make.bat
+++ examples/visualize/make.bat
@@ -3,7 +3,7 @@
 rem ===============        <><> Infinity Web Assembly SDK makefile (batch file for now)
 
 rem Release build optimization level
-set EXTRA_COMPILE_OPTIONS=-O0
+set EXTRA_COMPILE_OPTIONS=-O2 -D__wasi__
 
 rem ======= Configure =======
 
@@ -11,30 +11,31 @@ rem WASI delivers standard c/c++ runtime libraries & WASM tools.
 rem Latest wasi-sdk here : https://github.com/WebAssembly/wasi-sdk/releases
 rem Docs : https://wasi.dev
 
+set LLVM_H=..\..\sys\include
 set WASI_LIB=..\..\sys\wasi\lib\wasm32-wasi
 set WASI_H=..\..\sys\wasi\include
 set WASI_CPP_H=..\..\sys\wasi\include\c++\v1
-set WASI_BIN=..\..\sys\wasi\sdk\bin
+set WASM_TOOLS=..\..\sys\bin
+set CPP=%WASM_TOOLS%\clang++
 
 rem <><> Infinity/XSG DOM interop/bootstrap headers
 set INFINITY_H=..\..\sys\infinity
 
 set COMPILE_OPTIONS=%EXTRA_COMPILE_OPTIONS%  --target=wasm32 -fno-exceptions -c 
-set HEADERS=-I%WASI_H% -I%WASI_CPP_H% -I%INFINITY_H%
+set HEADERS=-I%WASI_CPP_H% -I%WASI_H% -I%LLVM_H% -I%INFINITY_H%
 
 rem ======= Compile =======
 
-mkdir obj
+if not exist obj mkdir obj
 
-%WASI_BIN%\clang %HEADERS% %COMPILE_OPTIONS% ./source/doodle.cpp    -o ./obj/doodle.o 
+%CPP% %HEADERS% %COMPILE_OPTIONS% ./source/doodle.cpp    -o ./obj/doodle.o 
 
 rem ======= Link =======
 
 
-set LINK_OPTIONS=-no-entry --allow-undefined --export=app_init --export=app_process --export=app_terminate --export=app_move --export=app_button
+set LINK_OPTIONS=--no-entry --allow-undefined --export=app_init --export=app_process --export=app_terminate --export=app_move --export=app_button
 set LIBS= %WASI_LIB%\libc.a  %WASI_LIB%\libc++abi.a   %WASI_LIB%\libc++.a 
-
-%WASI_BIN%\wasm-ld %LINK_OPTIONS%  %LIBS% ./obj/doodle.o    -o ./obj/app.wasm
+%WASM_TOOLS%\wasm-ld %LINK_OPTIONS%  %LIBS% ./obj/doodle.o    -o ./obj/app.wasm
 
 
 rem ======= Inspect =======
