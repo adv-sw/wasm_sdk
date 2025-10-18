1b34c7babb9b7d0f5cd6c2086d9e36f78aa7904b
Steve Williams <steve@advance-software.com>
2025-10-18 08:05:21 +0100

    fixed paths

--- examples/simple/run.bat
+++ examples/simple/run.bat
@@ -4,4 +4,4 @@ rem ===============        <><> Infinity Web Assembly SDK run script
 
 rem ======= Run =======
 
-infinity.exe -alone -desktop -ui=0 -rt=dxr %cd%\..\..\website\simple
+infinity.exe -alone -desktop -ui=0 -rt=1 ..\..\website\simple
--- examples/simple/run_safe.bat
+++ examples/simple/run_safe.bat
@@ -4,4 +4,4 @@ rem ===============        <><> Infinity Web Assembly SDK run script
 
 rem ======= Run =======
 
-infinity.exe -alone -desktop -ui=0 %cd%\..\..\website\simple
+infinity.exe -alone -desktop -ui=0 ..\..\website\simple
